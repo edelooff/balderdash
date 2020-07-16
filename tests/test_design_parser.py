cb7a93385bdbc2eb94635902b092b2595964c07e
Elmer de Looff <elmer.delooff@gmail.com>
2020-07-17 00:18:36 +0200

    Adds parser test suites.

---
+++ tests/test_design_parser.py
@@ -0,0 +1,85 @@
+from functools import partial
+from itertools import zip_longest
+from string import ascii_uppercase
+
+import pytest
+
+from balderdash.parser import (
+    get_parser,
+    create_design,
+    generate_designs,
+)
+from balderdash.types import Design
+
+
+@pytest.fixture
+def design_parser():
+    parser = get_parser("design")
+    return partial(create_design, parser)
+
+
+@pytest.mark.parametrize("name", ascii_uppercase)
+@pytest.mark.parametrize("size", "SL")
+@pytest.mark.parametrize("flower_count", [5, 12])
+def test_create_design_basics(design_parser, name, size, flower_count):
+    design = design_parser(f"{name}{size}{flower_count}")
+    assert isinstance(design, Design)
+    assert design.name == name
+    assert design.size == size
+    assert design.additional == flower_count
+
+
+@pytest.mark.parametrize(
+    "pattern, name, size, required, additional",
+    [
+        pytest.param("AL1a5", "A", "L", dict(a=1), 4, id="AL1a5"),
+        pytest.param("LL2a2b2c6", "L", "L", dict(a=2, b=2, c=2), 0, id="LL2a2b2c6"),
+        pytest.param("LL2a2b2c6", "L", "L", dict(a=2, b=2, c=2), 0, id="LL2a2b2c6"),
+    ],
+)
+def test_create_design_with_required(
+    design_parser, pattern, name, size, required, additional
+):
+    design = design_parser(pattern)
+    assert isinstance(design, Design)
+    assert design.name == name
+    assert design.size == size
+    for flower, count in design.required.items():
+        assert flower.size == design.size
+        assert required[flower.species] == count
+    assert design.additional == additional
+
+
+@pytest.mark.parametrize(
+    "pattern",
+    [
+        pytest.param("aL5", id="lowercase name"),
+        pytest.param("Al5", id="lowercase size"),
+        pytest.param("AX5", id="illegal size indicator"),
+        pytest.param("ALx5", id="missing flower count"),
+        pytest.param("AL 2a5", id="whitespace in design"),
+        pytest.param("AL-2a5", id="negative flower count"),
+        pytest.param("AL-5", id="negative total count"),
+    ],
+)
+def test_create_design_bad_syntax(design_parser, pattern):
+    with pytest.raises(ValueError, match="Bad design syntax"):
+        design_parser(pattern)
+
+
+def test_create_design_bad_total_quantity(design_parser):
+    with pytest.raises(ValueError, match="required flowers greater than total size"):
+        design_parser("AL2a2b3")
+
+
+def test_create_design_redefined_required_flower(design_parser):
+    with pytest.raises(ValueError, match="Multiple definitions"):
+        design_parser("AL2a2a5")
+
+
+def test_generate_designs():
+    patterns = ["AL1a5", "BS2x2y5"]
+    designs = list(generate_designs(patterns))
+    for design, pattern in zip_longest(designs, patterns):
+        assert isinstance(design, Design)
+        assert design.name == pattern[0]
---
+++ tests/test_flower_parser.py
@@ -0,0 +1,50 @@
+from functools import partial
+from itertools import product, zip_longest
+from string import ascii_lowercase
+
+import pytest
+
+from balderdash.parser import (
+    get_parser,
+    create_flower,
+    generate_flowers,
+)
+from balderdash.types import Flower
+
+
+@pytest.fixture
+def flower_parser():
+    parser = get_parser("flower")
+    return partial(create_flower, parser)
+
+
+@pytest.mark.parametrize("size", "SL")
+@pytest.mark.parametrize("species", ascii_lowercase)
+def test_parse_flower(flower_parser, species, size):
+    flower = flower_parser(f"{species}{size}")
+    assert isinstance(flower, Flower)
+    assert flower.species == species
+    assert flower.size == size
+
+
+@pytest.mark.parametrize(
+    "pattern",
+    [
+        pytest.param("AL", id="uppercase species"),
+        pytest.param("Al", id="lowercase size"),
+        pytest.param("A l", id="whitespace-1"),
+        pytest.param("Al ", id="whitespace-2"),
+    ],
+)
+def test_create_flower_bad_syntax(flower_parser, pattern):
+    with pytest.raises(ValueError, match="Bad flower syntax"):
+        flower_parser(pattern)
+
+
+def test_generate_flowers():
+    patterns = ["".join(flower) for flower in product(ascii_lowercase, "SL")]
+    flowers = list(generate_flowers(patterns))
+    for flower, (species, size) in zip_longest(flowers, patterns):
+        assert isinstance(flower, Flower)
+        assert flower.species == species
+        assert flower.size == size
