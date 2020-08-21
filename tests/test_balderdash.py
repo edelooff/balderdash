f68ef532425b79b35480afc94f99ba651e18e601
Elmer de Looff <elmer.delooff@gmail.com>
2020-08-22 00:35:26 +0200

    Adds test for entrypoint script, with small refactor to main test.

--- tests/test_balderdash.py
+++ tests/test_balderdash.py
@@ -1,11 +1,21 @@
 import sys
 from io import StringIO
+from subprocess import PIPE, Popen
 
 from balderdash import main
 
+DESIGNS_AND_FLOWERS = "AS1b2\nBL1x1\n\nbS\naS\nxL\nxL"
+EXPECTED_BOUQUETS = "AS1a1b\nBL1x\nBL1x\n"
+
 
 def test_main(capsys):
-    sys.stdin = StringIO("AS1b2\nBL1x1\n\nbS\naS\nxL\nxL")
+    sys.stdin = StringIO(DESIGNS_AND_FLOWERS)
     main()
     captured = capsys.readouterr()
-    assert captured.out == "AS1a1b\nBL1x\nBL1x\n"
+    assert captured.out == EXPECTED_BOUQUETS
+
+
+def test_entrypoint():
+    process = Popen('balderdash', stdin=PIPE, stdout=PIPE, text=True)
+    out, err = process.communicate(DESIGNS_AND_FLOWERS)
+    assert out == EXPECTED_BOUQUETS
